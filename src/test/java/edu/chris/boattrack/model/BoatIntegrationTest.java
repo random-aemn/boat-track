203bb8beb28500f61c7e1009df1be1a5d21a3c3d
csquared <csquared@csquared-01.home>
2023-03-14 20:37:10 -0400

    Enhanced data creation for testing.

--- src/test/java/edu/chris/boattrack/model/BoatIntegrationTest.java
+++ src/test/java/edu/chris/boattrack/model/BoatIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 import edu.chris.boattrack.model.id.BoatId;
 import edu.chris.boattrack.repository.BoatJpaRepository;
+import jakarta.transaction.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BoatIntegrationTest {
@@ -28,17 +29,66 @@ class BoatIntegrationTest {
 
 
     private static RestTemplate restTemplate = null;
+    
+    private static final int SN_HULL_BASE_VALUE=700;
+    private static final int CG_HULL_BASE_VALUE=50;
+    private static final int DD_HULL_BASE_VALUE=960;
+    
+    private  void createSubTestRecords() {
+    	for (int i=SN_HULL_BASE_VALUE; i<SN_HULL_BASE_VALUE+3; i++) {
+    		BoatId boatId = new BoatId("SSN", ((Integer)i).toString());
+    		Boat boat = new Boat(boatId, 80000 + i*100);
+    		bjr.save(boat);
+    		/*
+    		 * 80000 + 700*100 = 80000 + 70000 = 150000
+    		 * 150100
+    		 * 150200 
+    		 */
+    	}
+    }
+    
+    private  void createCgTestRecords() {
+    	for (int i=CG_HULL_BASE_VALUE; i<CG_HULL_BASE_VALUE+2; i++) {
+    		BoatId boatId = new BoatId("CG", ((Integer)i).toString());
+    		Boat boat = new Boat(boatId, 50000 + i*100);
+    		bjr.save(boat);
+    		/*
+    		 * 50000 + 50*100 = 50000 + 5000 = 55000 
+    		 * 55100
+    		 */
+    	}
+    }
+    
+    private  void createDdTestRecords() {
+    	for (int i=DD_HULL_BASE_VALUE; i<DD_HULL_BASE_VALUE+1; i++) {
+    		BoatId boatId = new BoatId("DD", ((Integer)i).toString());
+    		Boat boat = new Boat(boatId, 10000 + i*100);
+    		/*
+    		 * 10000 + 960*100 = 10000 + 96000 = 106000 
+    		 */
+    		bjr.save(boat);
+    	}
+    }
+    
+    private void createTestData() {
+    	createSubTestRecords();
+    	createCgTestRecords();
+    	createDdTestRecords();
+    	
+    }
 
     @BeforeAll
+    
     public static void init() {
         restTemplate = new RestTemplate();
     }
+    
 	@Test
+	@Transactional
 	void testBoatPersistanceAndRetrieval() {
-		boolean testResult = false;
-		BoatId boatId = new BoatId("SSN", "701");
-		Boat boat = new Boat(boatId, 80000);
-		bjr.save(boat);
+		createTestData();
+		BoatId boatId = new BoatId("SSN", "700");
+		Boat boat = new Boat(boatId, 150000);
 		
 		Optional<Boat> optShadow = bjr.findById(boatId);
 		Boat shadow = null;
