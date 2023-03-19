6f1bdc0915750cc14898e489f0c7dfed4b6e6b12
csquared <csquared@csquared-01.home>
2023-03-19 10:39:40 -0400

    Additional tests; add method to BoatJpaRepository

--- src/main/java/edu/chris/boattrack/repository/BoatJpaRepository.java
+++ src/main/java/edu/chris/boattrack/repository/BoatJpaRepository.java
@@ -1,5 +1,8 @@
 package edu.chris.boattrack.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -7,6 +10,8 @@ import edu.chris.boattrack.model.Boat;
 import edu.chris.boattrack.model.id.BoatId;
 
 public interface BoatJpaRepository extends JpaRepository <Boat, BoatId>{
+	
+	Optional<List<Boat>> findByBoatIdShipClass (String shipClass);
 
 }
 
