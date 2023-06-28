9c6606938f7f3e20075387475c3e43001f4e27c9
random-aemn <82000272+random-aemn@users.noreply.github.com>
2023-06-28 08:23:16 -0400

    Attempt to commit TestEndpoints to remote

---
+++ src/test/java/edu/chris/boattrack/controller/rest/TestEndpoints.java
@@ -0,0 +1,8 @@
+package edu.chris.boattrack.controller.rest;
+
+public class TestEndpoints {
+
+	public static final String STATIC_EP = "/bt/v1/boat-test-message";
+	public static final String NO_SUCH_BOAT_EP = "/bt/v1/boat/xxx-000";
+	public static final String ADD_NEW_BOAT_EP = "bt/v1/boat";
+}
